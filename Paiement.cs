534e46b56416d1cc3cef0e6cb8ab541644dfa9d3
abderrafia <abderrafia.yahia@monboreal.ca>
2024-10-25 15:59:28 -0400

    Ajout de la classe abstraite Paiement avec propriétés et méthode abstraite

---
+++ Paiement.cs
@@ -0,0 +1,13 @@
+﻿public abstract class Paiement
+{
+    public double montant { get; set; }
+    public string description { get; set; }
+
+    public Paiement(double m, string d)
+    {
+        montant = m;
+        description = d;
+    }
+
+    public abstract void AfficherDetails();
+}
