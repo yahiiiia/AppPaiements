fd03e28ca2c99cbc1e50a5f3fc0bdd7c2f26906a
abderrafia <abderrafia.yahia@monboreal.ca>
2024-10-25 18:23:12 -0400

    Mise à jour de la classe Utilisateur avec les derniers changements

--- Utilisateur.cs
+++ Utilisateur.cs
@@ -5,37 +5,43 @@ using System.Collections.Generic;
 class Utilisateur
 {
     // Le nom de l'utilisateur
-    public string nom;
+    public string Nom { get; private set; }
 
     // La liste qui garde tous les paiements
-    public List<Paiement> listePaiements;
+    public List<Paiement> ListePaiements { get; private set; }
 
     // Quand on crée un nouvel utilisateur
-    public Utilisateur(string n)
+    public Utilisateur(string nom)
     {
-        nom = n;
+        Nom = nom;
         // On crée une nouvelle liste vide pour les paiements
-        listePaiements = new List<Paiement>();
+        ListePaiements = new List<Paiement>();
     }
 
     // Pour afficher les informations de l'utilisateur
     public void Afficher()
     {
-        Console.WriteLine("Bonjour " + nom + "!");
-        Console.WriteLine("Vous avez fait " + listePaiements.Count + " paiements.");
-
-        // On met une ligne pour faire joli
+        Console.WriteLine($"Bonjour {Nom} !"); // Hello {Name}!
+        Console.WriteLine($"Vous avez effectué {ListePaiements.Count} paiements."); // You have made {Count} payments.
         Console.WriteLine("------------------");
     }
 
     // Pour ajouter un nouveau paiement
-    public void AjouterPaiement(Paiement p)
+    public void AjouterPaiement(Paiement paiement)
     {
         // On ajoute le paiement à la liste
-        listePaiements.Add(p);
-
-        // On confirme l'ajout du paiement
-        Console.WriteLine("Super! Un nouveau paiement a été ajouté pour " + nom + ".");
+        ListePaiements.Add(paiement);
+        Console.WriteLine($"Génial ! Un nouveau paiement a été enregistré pour {Nom}."); // Awesome! A new payment has been recorded for {Name}.
         Console.WriteLine("-------------------");
+    }
+
+    // Pour afficher tous les paiements
+    public void AfficherTousLesPaiements()
+    {
+        Console.WriteLine("Voici les détails de tous les paiements :"); // Here are the details of all payments:
+        foreach (var paiement in ListePaiements)
+        {
+            paiement.AfficherDetails(); // Display the details of each payment
+        }
     }
 }
